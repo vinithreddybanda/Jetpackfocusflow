36f018c3b022adbecb90cbde09931952fa749283
Vinith Reddy Banda <116493221+vinithreddybanda@users.noreply.github.com>
2024-05-03 00:26:38 +0530

    init

---
+++ app/src/main/java/com/vinith/focusflow/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.vinith.focusflow.ui.theme
+
+import android.hardware.camera2.params.BlackLevelPattern
+import androidx.compose.ui.graphics.Color
+
+val White = Color(0xFFF9F8FB)
+val Grey = Color(0xE0E0E0E0)
+val Black = Color(0xFF000000)
